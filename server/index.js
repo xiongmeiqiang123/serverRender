bdc92c223188c5d78518a7be207e86e9d2d2aedd
xiongmeiqiang <xiongmeiqiang@xiaomi.com>
2018-03-12 13:28:35 +0800

    udpate

--- server/index.js
+++ server/index.js
@@ -9,6 +9,13 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(bodyParser.json({limit: '1mb'})) //解析post数据
 app.use(cors())
 
+router.use(function(req, res, next) {
+    console.log("Time:", Date.now());
+    console.log("Request URL:", req.originalUrl);
+    console.log('Request Type:', req.method);
+    next();
+});
+
 let _app = route(app)
 var server = _app.listen(7001, function() {
 	console.log('Listening on port %d', server.address().port);
