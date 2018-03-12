2db10e845e0a69f3badd33cf4847694188c8173b
xiongmeiqiang <xiongmeiqiang@xiaomi.com>
2018-03-12 13:33:00 +0800

    update

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.use(bodyParser.json({limit: '1mb'})) //解析post数据
 app.use(cors())
 
-router.use(function(req, res, next) {
+app.use(function(req, res, next) {
     console.log("Time:", Date.now());
     console.log("Request URL:", req.originalUrl);
     console.log('Request Type:', req.method);
